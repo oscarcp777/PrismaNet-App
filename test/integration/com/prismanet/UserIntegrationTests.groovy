136ab55ab37e089e0fca383bd495e0c3fabf5912
Santiago Donikian <santiagodonikian@gmail.com>
2013-05-17 10:34:23 -0300

    test de integracion agregado, testea un update de un atributo

--- test/integration/com/prismanet/UserIntegrationTests.groovy
+++ test/integration/com/prismanet/UserIntegrationTests.groovy
@@ -21,8 +21,23 @@ class UserIntegrationTests {
         def user = new User(userId: 'oscar', password: 'fiuba')
         assertNotNull user.save()
         assertNotNull user.id
+
         def foundUser = User.get(user.id)
         assertEquals 'oscar', foundUser.userId
     }
+
+    @Test
+    void testSaveAndUpdate() {
+        def user = new User(userId: 'oscar', password: 'fiuba')
+        assertNotNull user.save()
+
+        def foundUser = User.get(user.id)
+        foundUser.password = 'nuevaPass'
+        foundUser.save()
+
+        def editedUser = User.get(user.id)
+        assertEquals 'nuevaPass', editedUser.password
+    }
+
 
 }
