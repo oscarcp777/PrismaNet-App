77f15ef5afd2a979a51f65a86219d80f3986177b
sdonik <santiagodonikian@gmail.com>
2013-09-23 17:42:40 -0300

    Job contra base mongo andando

--- grails-app/jobs/com/prismanet/TweetJob.groovy
+++ grails-app/jobs/com/prismanet/TweetJob.groovy
@@ -1,57 +1,57 @@
 package com.prismanet
 
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
+import groovy.time.TimeCategory
 
+import org.codehaus.groovy.grails.web.json.JSONObject
 
+import twitter4j.internal.org.json.JSONObject;
+import twitter4j.StallWarning
+import twitter4j.Status
+import twitter4j.StatusDeletionNotice
+import twitter4j.StatusListener
+import twitter4j.internal.json.StatusJSONImpl
 
-class TweetJob implements StatusListener{
+import com.mongodb.BasicDBObject
+import com.mongodb.DB
+import com.mongodb.DBCollection
+import com.mongodb.DBCursor
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientURI
+
+class TweetJob {
 
 	def tweetService
 	def group = "tweetsJobs"
+	
+	MongoClient client 
+	
+	public TweetJob(){
+		client = new MongoClient(new MongoClientURI("mongodb://localhost"))
+	}
 
 	static triggers = {
-		simple repeatInterval: 600000l, repeatCount:-1 , startDelay: 30000
+		simple repeatInterval: 60000l, repeatCount:-1 , startDelay: 30000
 	}
 
 	def execute() {
 		print "Starting the Tweets job."
-		tweetService.streamConection(this)
+		DB db = client.getDB("prismanet")
+		DBCollection tweets = db.getCollection("tweets")
+		use ( TimeCategory ) {
+			Date filteredDate = new Date()-1.minute
+			print "fecha a filtrar: " + filteredDate
+			def filters = [created_at:['$gte': filteredDate]]
+			DBCursor cursor = tweets.find(filters as BasicDBObject)
+			for (BasicDBObject tweet : cursor){
+				JSONObject obj = new JSONObject(tweet)
+				Status status = new StatusJSONImpl(obj)
+				System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+				tweetService.saveTweet(status)
+			}
+			cursor.close()
+		}
 		println "Running Job!" + new Date()
 	}
 	
 	
-	
-	@Override
-	public void onStatus(Status status) {
-		System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-		tweetService.saveTweet(status)
-	}
-
-	@Override
-	public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-		System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-	}
-
-	@Override
-	public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-		System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-	}
-
-	@Override
-	public void onScrubGeo(long userId, long upToStatusId) {
-		System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);
-	}
-
-	@Override
-	public void onStallWarning(StallWarning warning) {
-		System.out.println("Got stall warning:" + warning);
-	}
-
-	@Override
-	public void onException(Exception ex) {
-		ex.printStackTrace();
-	}
 }
