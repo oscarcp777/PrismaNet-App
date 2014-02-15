b1c3e50e16f6ecf32c6db453c75f57d83307cea4
oscar <caceres.oscar7@gmail.com>
2014-02-15 14:57:47 -0300

    Template para probar vistas

---
+++ template/index/js/chart/prismaChart.js
@@ -0,0 +1,353 @@
+var doRequest = function(url,data,callback, errorHandler, method) {
+	      $.ajax({
+	              url: url,
+	              type: method,
+	              data: data,
+	              cache: false,
+	              success: callback,
+	              error: errorHandler
+	            });
+};
+
+function inicializeColorChar(){
+	Highcharts.getOptions().colors = Highcharts.map(Highcharts
+			.getOptions().colors, function(color) {
+		return {
+			radialGradient : {
+				cx : 0.5,
+				cy : 0.3,
+				r : 0.7
+			},
+			stops : [
+					[ 0, color ],
+					[
+							1,
+							Highcharts.Color(color).brighten(-0.3).get(
+									'rgb') ] // darken
+			]
+		};
+	});
+}
+
+function getTweetCharPie(){
+	doRequest('conceptTweetsJson',null,paintCharPie, null, 'GET');
+}
+function getConceptRealTime(id, div){
+	var data = {"id":id, "div":div}
+	doRequest('../conceptsRealTime',data,printRealTimeChar, null, 'GET');
+}
+function getConceptCharDay(id, div){
+	var data = {"id":id, "div":div}
+	doRequest('../conceptsDayJson',data,paintCharLine, null, 'GET');
+}
+
+function getConceptCharDate(id, div){
+	var data = {"id":id, "div":div}
+	doRequest('../conceptsDateJson',data,paintCharLine, null, 'GET');
+}
+
+function getConceptCharHour(id, div){
+	var data = {"id":id, "div":div}
+	doRequest('../conceptsHourJson',data,paintCharLine, null, 'GET');
+}
+
+function getConceptCharMinute(id, div){
+	var data = {"id":id, "div":div}
+	doRequest('../conceptsMinuteJson',data,paintCharLine, null, 'GET');
+}
+
+function getUserConceptsLineChart(div){
+	var data = {"div":div}
+	doRequest('getComparativeConceptChartByMinute',data,paintCharLine, null, 'GET');
+}
+
+function getUserConceptsLineChar(div){
+	var data = {"div":div}
+	doRequest('getComparativeConceptChartByHour',data,paintCharLine, null, 'GET');
+}
+
+function getUserConceptsLineChar(div){
+	var data = {"div":div}
+	doRequest('getComparativeConceptChartByDate',data,paintCharLine, null, 'GET');
+}
+
+function printRealTimeChar(data){
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+
+    var chart;
+    $(data.container).highcharts({
+        chart: {
+            type: 'spline',
+            animation: Highcharts.svg, // don't animate in old IE
+            plotShadow: true,
+            plotBorderWidth: 2,
+            events: {
+                load: function() {
+
+                    // set up the updating of the chart each second
+                    var series = this.series[0];
+                    setInterval(function() {
+                    	var dataNew = {"id":data.id}
+                    	doRequest('../conceptsRealTimeForOneMinute',
+                    			  dataNew,
+                    			 function(data) {
+                	        		for ( var int = 0; int < data.length; int++) {
+                	        			var element = data[int];
+                	        			series.addPoint(element, true, true);
+                	        			}
+                    			}
+                    			, null, 
+                    			'GET');
+                       
+                    }, 60000);
+                }
+            }
+        },
+        title: {
+            text:data.title
+        },
+        subtitle: {
+            text: data.subTitle
+        },
+        xAxis: {
+        	gridLineWidth: 1,
+        	title: {
+                text:data.titleX
+            },
+            type: 'datetime'
+        },
+        yAxis: {
+            title: {
+                text: data.titleY
+            },
+            min:0
+        },
+        tooltip: {
+            formatter: function() {
+                    return "Click para ver los <b> "+this.y+'</b> tweets de las '+ Highcharts.dateFormat('%H:%M', this.x) ;
+            },
+            crosshairs: true,
+	        shared: true
+        },
+        legend: {
+            enabled: true
+        },
+        exporting: {
+            enabled: true
+        },
+        
+        plotOptions: {
+        	spline: {
+                lineWidth: 4,
+                states: {
+                    hover: {
+                        lineWidth: 6
+                    }
+                }
+            },
+            series: {
+                cursor: 'pointer',
+                point: {
+                    events: {
+                        click: function() {
+                        	window.location.href="../../tweet/list?conceptsId="+data.id+"&"+data.dateProp+"="+this.x;
+                        }
+                    }
+                },
+                marker: {
+		    		fillColor: 'white',
+		    		lineWidth: 3,
+		    		lineColor: Highcharts.getOptions().colors[0]
+		    	},
+		    	zIndex: 2
+            }
+        },
+        
+       
+        series: [{
+            name: data.title,
+            data:data.data
+	    	
+        }
+        ]
+    });
+}
+
+
+function paintCharPie(dataJson) {
+	// Radialize the colors
+	
+
+	// Build the chart
+	$('#container')
+			.highcharts(
+					{
+						chart : {
+							 animation: Highcharts.svg, // don't animate in old IE
+					            plotShadow: true,
+					            plotBorderWidth: 2
+						},
+						title : {
+							text : 'Porcentajes de tweets por Concepto'
+						},
+						tooltip : {
+							pointFormat : '{series.name}: <b>{point.y}</b>'
+						},
+						plotOptions : {
+							pie : {
+								allowPointSelect : true,
+								showInLegend: true,
+								cursor : 'pointer',
+								dataLabels : {
+									enabled : true,
+									color : '#000000',
+									connectorColor : '#000000',
+									formatter : function() {
+										return '<b>' + this.point.name
+												+ '</b>: '
+												+ this.percentage.toFixed(2);
+												+ ' %';
+									}
+								}
+							}
+						},
+						series : [ {
+							type : 'pie',
+							name : 'Tweets',
+							data : dataJson
+						} ]
+					});
+
+}
+
+
+function paintCharLine(data){
+	Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
+	 $(data.container).highcharts({
+	        chart: {
+	            type: 'spline',
+	            animation: Highcharts.svg, // don't animate in old IE
+	            plotShadow: true,
+	            plotBorderWidth: 2
+	        },
+	        title: {
+	            text:data.title
+	        },
+	        subtitle: {
+	            text: data.subTitle
+	        },
+	        xAxis: {
+	        	gridLineWidth: 1,
+//	            lineColor: '#1ABC9C',
+//	            tickColor: '#1ABC9C',
+	        	title: {
+	                text:data.titleX
+	            },
+	            type: 'datetime',
+                labels: {
+                    overflow: 'justify'
+                }
+//	            tickPixelInterval:3600,
+//	            plotLines: [{
+//	                value:0,
+//	            }]
+	        },
+	        yAxis: {
+	            title: {
+	                text: data.titleY
+	            },
+	            min:0
+	        },
+	        tooltip: {
+	            formatter: function() {
+                    return "Click para ver los <b> "+this.y+'</b> tweets de las '+ Highcharts.dateFormat('%d/%m/%Y--%H:%M', this.x) ;
+	            },
+	            crosshairs: true,
+		        shared: false
+	        },
+	        legend: {
+	            enabled: true
+	        },
+	        exporting: {
+	            enabled: true
+	        },
+	        plotOptions: {
+//	        	spline: {
+//	                lineWidth: 4,
+//	                states: {
+//	                    hover: {
+//	                        lineWidth: 6
+//	                    }
+//	                }
+//	            },
+	            series: {
+	                cursor: 'pointer',
+	                point: {
+	                    events: {
+	                        click: function() {
+	                        	window.location.href=data.cursorEvent+this.x;
+	                        }
+	                    }
+	                }
+//	            ,
+//	                marker: {
+//			    		fillColor: 'white',
+//			    		lineWidth: 3,
+//			    		lineColor: Highcharts.getOptions().colors[0]
+//			    	},
+//			    	zIndex: 2
+	            }
+	        },
+	        series: data.series
+//	        series: [{
+//	            name: data.title,
+//	            data:data.data,
+//		    	marker: {
+//		    		fillColor: 'white',
+//		    		lineWidth: 3,
+//		    		lineColor: Highcharts.getOptions().colors[0]
+//		    	},
+//		    	zIndex: 2
+//	        }
+//	        ]
+	    });
+}
+
+
+
+function paintCharGender() {
+	$('.easy-pie-chart.percentage')
+			.each(
+					function() {
+						var $box = $(this).closest('.infobox');
+						var barColor = $(this).data('color')
+								|| (!$box.hasClass('infobox-dark') ? $box
+										.css('color')
+										: 'rgba(255,255,255,0.95)');
+						var trackColor = barColor == 'rgba(255,255,255,0.95)' ? 'rgba(255,255,255,0.25)'
+								: '#E2E2E2';
+						var size = parseInt($(this).data('size')) || 300;
+						$(this)
+								.easyPieChart(
+										{
+											barColor : barColor,
+											trackColor : trackColor,
+											scaleColor : false,
+											lineCap : 'butt',
+											lineWidth : parseInt(size / 10),
+											animate : /msie\s*(8|7|6)/
+													.test(navigator.userAgent
+															.toLowerCase()) ? false
+													: 1000,
+											size : size
+										});
+					})
+}
---
+++ template/index/js/home.js
@@ -0,0 +1,71 @@
+/**
+
+ *Jquery para marcar como active un elemento del menu lateral 
+ */
+function activeItemMenuLevel2(item,subItem,nameItem){
+	editClassItem(item,'active','active open');
+	editClassItem(subItem,'active','active');
+	$('#nameItem').text(nameItem);
+}
+function activeItemMenuLevel3(item,level1,level2,nameItem){
+	editClassItem(item,'active','active open');
+	editClassItem(level1,'active','active open');
+	editClassItem(level2,'active','active');
+	$('#nameItem').text(nameItem);
+}
+function activeItemMenuLevel1(item,nameItem){
+	editClassItem(item,'active','active');
+	$('#nameItem').text(nameItem);
+}
+function editClassItem(item,classRemove,classNew){
+	$("#"+item).siblings('.'+classRemove).removeClass(classRemove);
+	$("#"+item).addClass(classNew);
+}
+
+function changeState(id, url, tweetId, conceptId, value){
+	var parent=id.substring(0,id.length-3);
+	var className = $("#"+id).attr('class');
+	if(className.indexOf("selected")!=-1){
+		$("#"+id).removeClass("selectedPOS")
+		$("#"+id).removeClass("selectedNEQ")
+		$("#"+id).removeClass("selectedNEU")
+	}else{
+		$("#"+parent +" > a").removeClass("selectedPOS")
+		$("#"+parent +" > a").removeClass("selectedNEQ")
+		$("#"+parent +" > a").removeClass("selectedNEU")
+		if(value=="POS")
+			$("#"+id).addClass("selectedPOS");
+		if(value=="NEG")
+			$("#"+id).addClass("selectedNEQ");
+		if(value=="NEU")
+			$("#"+id).addClass("selectedNEU");
+		
+	}
+	data = {'conceptId': conceptId, 'tweetId': tweetId, 'value':value}
+    doRequest(url, data, null, null, "POST");
+}
+
+function activeOpinion(id, value){
+	if(value == "POSITIVE"){
+		$("#"+id+"Pos").addClass("selectedPOS");
+	}
+	if(value == "NEGATIVE"){
+		$("#"+id+"Neg").addClass("selectedNEQ");
+	}
+	if(value == "NEUTRAL"){
+		$("#"+id+"Que").addClass("selectedNEU");
+	}
+		
+}
+
+function doRequest(url,data,callback, errorHandler, method) {
+    $.ajax({
+            url: url,
+            type: method,
+            data: data,
+            cache: false,
+            success: callback,
+            error: errorHandler
+          });
+};
+
