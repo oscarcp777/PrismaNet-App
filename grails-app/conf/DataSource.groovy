d92978bda610c838decd7eb91d0635b131de3097
oscar <caceres.oscar7@gmail.com>
2014-04-05 20:52:01 -0300

    pool de coneccciones

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,11 +29,30 @@ environments {
 			pooled = true
 			dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:mysql://localhost:3306/prismanet"
+            url = "jdbc:mysql://localhost:3306/prismanet?autoReconnect=true"
 			driverClassName = "com.mysql.jdbc.Driver"
 			username = "root"
 			password = "fiuba"
-			//url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+			properties {
+               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               jdbcInterceptors = "ConnectionState;StatementCache(max=200)"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
 		}
 		grails {
 			mongo {
