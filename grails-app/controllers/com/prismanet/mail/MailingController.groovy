8709bcbe1c6832b17ad071a14f78821486f2fb8c
sdonik <sdonikian@fi.uba.ar>
2015-03-18 01:14:17 -0300

    Update MailingController.groovy
    Datos de la semana  11-3 a 17-3

--- grails-app/controllers/com/prismanet/mail/MailingController.groovy
+++ grails-app/controllers/com/prismanet/mail/MailingController.groovy
@@ -13,58 +13,58 @@ class MailingController {
 	}
 	private def getData(){
 		def data =[:]
-		data.from='04/03'
-		data.to='10/03'
-		data.tentFrom='25/02'
-		data.tentTo='03/03'
+		data.from='11/03'
+		data.to='17/03'
+		data.tentFrom='04/03'
+		data.tentTo='10/03'
 		
-		data.tweets1=30626
-		data.author1=9766
-		data.day1='08/03'
-		data.wordsDay1='cambio, mariuvidal, gente, mmxv, presidente, pro, felizcumplepresidentedelcambio, mm2015'
-		data.hour1='08/03 12hs'
-		data.wordsHour1='mariuvidal, cambio, mmxv, felizcumplepresidentedelcambio, presidente, gente, pro'
-		data.tent1='-7.53%'
-		data.postLikes1=85532
-		data.postComments1=3520
-		data.postLink1='https://www.facebook.com/55432788477/posts/10153179562208478'
-		data.postPhoto1='https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xaf1/v/t1.0-9/p480x480/11050792_10153179558848478_8492985502725731055_n.png?oh=86968fe237c8cbbc3decc8aa95a0c4b3&oe=558E2802&__gda__=1438448631_724acb58b8a42db87cc7ac62432bc23f'
+		data.tweets1=47472
+		data.author1=15218
+		data.day1='17/03'
+		data.wordsDay1='argentina, presidente, sanzernesto, cambio, si, danielscioli, pro, elisacarrio, horaciorlarreta, mariuvidal, cepo, radiomitre, proargentina, sergiomassa'
+		data.hour1='16/03 12hs'
+		data.wordsHour1='cambio, sanzernesto, elisacarrio, mariuvidal, presidente, proargentina, mauricioyvos, futuro, pro, equipo, horaciorlarreta'
+		data.tent1='55%'
+		data.postLikes1=38356 
+		data.postComments1=1823
+		data.postLink1='https://www.facebook.com/55432788477/posts/10153193620693478'
+		data.postPhoto1='https://fbcdn-sphotos-a-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/s480x480/10420753_10153193617358478_4227045661516428873_n.jpg?oh=b1498f9bf36a5523a5c036312c0ae827&oe=55B62173&__gda__=1437524364_3fb489fcfce8bd3e37053807b5fa965d'
 		
-		data.tweets2=36853
-		data.author2=11515
-		data.day2='10/03'
-		data.wordsDay2='sciolienc5n, c5n, gatosylvestre, gobernador, naranja, provincia, anibalpittelli, escuelas, acollia, stolbizer, propaganda'
-		data.hour2='05/03 21hs'
-		data.wordsHour2='minsaurralde, junto, 300, primeros, policíalocal, lomasdezamora, efectivos, funciones, egreso, ciurcac'
-		data.tent2='3,97%'
-		data.postLikes2=10327
-		data.postComments2=352
-		data.postLink2='https://www.facebook.com/danielsciolioficial/photos/a.104999958786.98272.39940248786/10153224035938787/?type=1&theater'
-		data.postPhoto2='https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/s720x720/11043056_10153224035938787_3030651588692371339_n.jpg?oh=70cfeede7a8522413c11ab3d3fb3879b&oe=557BF657&__gda__=1438216457_978a5dd2fdcec95060aac3f1c08f5233'
+		data.tweets2=39888
+		data.author2=9551
+		data.day2='12/03'
+		data.wordsDay2='sciolienplanm, maximmontenegro, olanaranjamza, prensaciurca, taller, canal26noticias, randazzof, jovenesxscioli, pymes, planm, presidente, social'
+		data.hour2='15/03 22hs'
+		data.wordsHour2='scioliconmajul, lacornisaok, majulluis, americatv, randazzof, presidente, acollia'
+		data.tent2='8.23%'
+		data.postLikes2=4328
+		data.postComments2=288
+		data.postLink2='https://www.facebook.com/39940248786/posts/10153234814818787'
+		data.postPhoto2='https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/s851x315/10981537_10153234813228787_5917119666945034665_n.jpg?oh=466daec46c84aa524f8c15abac2b0f22&oe=55B46B4D&__gda__=1438065588_59fb36f9d16d24294ea89f6c30abc969'
 
-		data.tweets3=20873
-		data.author3=7391
-		data.day3='08/03'
-		data.wordsDay3='massaenlacornisa, massaprensa, lacornisaok, majulluis, americatv, mauriciomacri, presidente'
-		data.hour3='08/03 22hs'
-		data.wordsHour3='massaenlacornisa, massaprensa, lacornisaok, majulluis, americatv, presidente, perpetua, justicia'
-		data.tent3='-15.42%'
-		data.postLikes3=20157
-		data.postComments3=935
-		data.postLink3='https://www.facebook.com/374082229927/posts/10153310903174928'
-		data.postPhoto3='https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/s480x480/10384751_10153310902989928_8883112970343545771_n.jpg?oh=4053c0e188d5bcd8de6a712010d4f8e7&oe=55897884&__gda__=1433997691_6224f1962eb207537995727fa6ea4dce'
+		data.tweets3=33855
+		data.author3=9559
+		data.day3='12/03'
+		data.wordsDay3='massaprensa, massaconfantino, fantinofantino, presidente, animalessueltos, animalesoficial, argentina, mauriciomacri'
+		data.hour3='12/03 00hs'
+		data.wordsHour3='massaconfantino, massaprensa, fantinofantino, presidente, animalessueltos, animalesoficial, vuelta'
+		data.tent3='62.2%'
+		data.postLikes3=6175
+		data.postComments3=422
+		data.postLink3='https://www.facebook.com/374082229927/posts/10153338282869928'
+		data.postPhoto3='https://scontent-gru.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/s851x315/11073573_10153338282549928_8990146294847614603_n.jpg?oh=93ed2528d29c54406671a188a25d4686&oe=55B3BCDDoh=4053c0e188d5bcd8de6a712010d4f8e7&oe=55897884&__gda__=1433997691_6224f1962eb207537995727fa6ea4dce'
 
-		data.tweets4=24363
-		data.author4=8466
-		data.day4='05/03'
-		data.wordsDay4='cfkargentina, randazzoen678, 678elprograma, nuevos, trenroca, coches, trenes, roca, sueños, viejos, fierros, trenesargentinos'
-		data.hour4='05/03 21hs'
-		data.wordsHour4='randazzoen678, 678elprograma, cfkargentina, todoscflorencio, presidente, tv_publica, lealtad, trenes, tren, candidato, prensainterior'
-		data.tent4='2.56%'
-		data.postLikes4=40282
-		data.postComments4=1879
-		data.postLink4='https://www.facebook.com/125621324134352/posts/990249807671495'
-		data.postPhoto4='https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xpa1/v/t1.0-9/10941034_990246127671863_1324178622215605428_n.jpg?oh=f207a49ac0487c688e1b233f4028e1dd&oe=55769B2B&__gda__=1433942163_2220b04599c0fc19b7dccf9e555108d7'
+		data.tweets4=26213
+		data.author4=8612
+		data.day4='12/03'
+		data.wordsDay4='prensarandazzo, danielscioli, todoscflorencio, conferencia, medios, tren, casarosadaar, prensa, cordoba, candidato, cfkargentina, trenes'
+		data.hour4='13/03 00hs'
+		data.wordsHour4='prensarandazzo, c5n, apetchecopar_ok, cfkargentina, clarín, randazzoenc5n, tren, país, baby, cordoba'
+		data.tent4='7.59%'
+		data.postLikes4=4328
+		data.postComments4=588
+		data.postLink4='https://www.facebook.com/125621324134352/posts/995485350481274'
+		data.postPhoto4='https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/s480x480/10522559_995484643814678_5907636100978866577_n.jpg?oh=02d6bdb92719700af7dc8fc66e288053&oe=5571A530&__gda__=1434870638_da8b03953639d6e35daa97cc4f826d69'
 
 		
 		data
