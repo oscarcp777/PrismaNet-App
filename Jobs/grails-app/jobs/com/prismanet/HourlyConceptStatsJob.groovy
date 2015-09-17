bdb15ba5d8d7076bd1fd8669c586964d42257486
Santiago <santiagodonikian@gmail.com>
2015-09-16 21:37:53 -0300

    Textos random para tweet

--- Jobs/grails-app/jobs/com/prismanet/HourlyConceptStatsJob.groovy
+++ Jobs/grails-app/jobs/com/prismanet/HourlyConceptStatsJob.groovy
@@ -21,14 +21,34 @@ class HourlyConceptStatsJob {
 		Date currentDate = new Date()
 		log.info "HourlyJob ejecutado: " + currentDate
 		hourlyConceptStatsService.loadStats()
+		String tweetText
 		use(TimeCategory){
 			currentDate = currentDate - 1.hour
+			tweetText = getRandomText(currentDate[Calendar.HOUR].toString())
 		}
 		String hourParameter = DateUtils.getDateFormat(DateTypes.HOUR_SIMPLIFIED, currentDate)
-		tweetService.sendTweet(
-			"Mira que paso en la última hora en al ambiente político "+
-				"info.prisma-net.com.ar/public/report?dt="+hourParameter
+		tweetService.sendTweet(tweetText +
+				" info.prisma-net.com.ar/public/report?dt="+hourParameter
 		)
+	}
+	
+	
+	private String getRandomText(String hour){
+		def vector = ["Mirá que paso en la última hora en al ambiente político",
+					  "Reporte de las " + hour +"hs",
+					  "Resumen de las " + hour +"hs",
+					  "Esto fue lo mas importante de las " + hour +"hs",
+					  "Mirá lo mas importante de las " + hour +"hs",
+					  "#presidenciales #politica No te pierdas este resumen",
+					  "Mira quienes, como y cuanto se habló de política en esta hora",
+					  "Panorama político de las " + hour +"hs",
+					  "#elecciones queres saber como viene el panorama político?",
+					  "Mirá en 2 minutos lo que pasó en un hora"]
+		
+		Random rand = new Random()
+		int max = vector.size()
+		vector[rand.nextInt(max)]
+		
 	}
 	
 }
